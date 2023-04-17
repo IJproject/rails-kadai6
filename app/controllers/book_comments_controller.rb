8ebcd61d190520c87200a7c1947aff07e3c03df9
EC2 Default User <ec2-user@ip-172-31-47-241.ap-northeast-1.compute.internal>
2023-04-17 07:32:25 +0000

    [finish] kadai6-all

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,17 +1,19 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = BookComment.new(comment_params)
     comment.user_id = current_user.id
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_to book_path(book)
+    @comment = BookComment.new
+    @comments = BookComment.all
   end
   
   def destroy
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     BookComment.find(params[:id]).destroy
-    redirect_to book_path(book)
+    @comment = BookComment.new
+    @comments = BookComment.all
   end
   
   private
